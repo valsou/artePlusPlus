fb11ca0c78f4ec205dd965ebb01e30a327dd323c
Valentin MEZIN <valslite@gmail.com>
2023-01-25 23:59:06 +0800

    Change for v2 API and hotfix
    Hotfix:
    arte.tv does not provide anymore link for a direct .mp4 file. User need to copy/paste the .m3u8 (playlist) file to youtube-dl, in addition of ffmpeg it will 1. Download the audio.mp4 and the video.mp4, 2. Merge both files into a .mp4.

--- artePlusPlus.user.js
+++ artePlusPlus.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Arte++
+// @name         Arte++ (dev)
 // @version      1.2.1
 // @description  Download videos of Arte.tv
 // @downloadURL  https://raw.githubusercontent.com/valsou/artePlusPlus/master/artePlusPlus.user.js
@@ -15,104 +15,75 @@
 'use strict';
 
 GM_addStyle(`
-.arte-plus-plus {
-padding-top: 60px;
-}
-
-.arte-plus-plus h1, h2 {
-font-family: "barna",sans-serif;
-}
-
-.arte-plus-plus h1 {
-font-size: 1.6rem;
-font-weight: 700;
+#arteplusplus {
+width: 100%;
+font-family: barna,sans-serif;
+font-weight: 500;
+line-height: 1.4;
+background-color: rgb(31,31,31);
 color: white;
-}
-
-.arte-plus-plus h2 {
-font-size: 1.4rem;
-color: white;
-}
-
-.arte-plus-plus .row {
-margin-bottom: 1.25rem;
-}
-
-.arte-plus-plus a {
-color: #9f9f9f;
-border: 0.05rem solid #535353;
-}
-
-.arte-plus-plus .row a,
-.arte-plus-plus .row span {
-font-size: 1.1rem;
-padding: .3rem;
 display: inline-block;
 }
 
-.arte-plus-plus .copypaste {
-background-color: #535353;
-color: #2F2F2F;
-border: 0.05rem solid #535353;
-cursor: pointer;
+#arteplusplus p {
+padding: 16px 0;
 }
 
-.arte-plus-plus .columns {
-margin-bottom : 0.5rem;
+.arteplusplus_link:after {
+content: "Copied";
+position: absolute;
+right: 0;
+color: #fff;
 }
 
-.link-plus-plus {
-display: inline-block;
+.arteplusplus_link {
+position: relative;
+background-color: rgb(70,70,70);
+color: rgb(40,40,40);
+border: 0;
+padding: 4px;
 }
 
-.link-plus-plus .copypaste:hover,
-.link-plus-plus .copypaste.copied {
-background-color: white;
-color: #2F2F2F;
-border-color: white;
+.arteplusplus_link.copied {
+background-color: rgb(250,72,28);
+color: white;
 }
 
-.link-plus-plus a:hover {
-background-color: white;
-color: #2F2F2F;
-border-color: white;
-}
-
-.link-plus-plus a:hover .copypaste {
-background-color: transparent;
-color: #9f9f9f;
+#arteplusplus a {
+color: white;
+text-decoration: underline;
 }`);
 
-const ARTE_API = 'https://api.arte.tv/api/player/v1/config/';
+const ARTE_API = 'https://api.arte.tv/api/player/v2/config/';
 const REGEX = /https:\/\/www.arte.tv\/(fr|de|en|es|pl|it)\/videos\/\d{6}-\d{3}-[A-Z]\//;
-const ELEMENT_TO_OBSERVE = document.querySelector('body');
-const PARENT_TO_APPEND = '#__next .container main h1';
+const LOCATION = window.location.pathname.split("/");
+const LANG = LOCATION[1];
+const VIDEO_ID = LOCATION[3];
+const ELEMENT_TO_OBSERVE = document.querySelector('main').children[1];
 const LOCALE = {
     'downloads': {
-        'fr_FR': 'Téléchargements',
-        'de_DE': 'Downloads',
-        'en_EN': 'Downloads',
-        'es_ES': 'Descargas',
-        'pl_PL': 'Pliki do pobrania',
-        'it_IT': 'Download'},
+        'fr': 'Téléchargements',
+        'de': 'Downloads',
+        'en': 'Downloads',
+        'es': 'Descargas',
+        'pl': 'Pliki do pobrania',
+        'it': 'Download'},
     'copy': {
-        'fr_FR': 'Copier',
-        'de_DE': 'Kopieren',
-        'en_EN': 'Copy',
-        'es_ES': 'Copiar',
-        'pl_PL': 'Kopiuj',
-        'it_IT': 'Copia'},
+        'fr': 'Copier',
+        'de': 'Kopieren',
+        'en': 'Copy',
+        'es': 'Copiar',
+        'pl': 'Kopiuj',
+        'it': 'Copia'},
     'copied': {
-        'fr_FR': 'Copié !',
-        'de_DE': 'Kopiert !',
-        'en_EN': 'Copied !',
-        'es_ES': 'Copiado !',
-        'pl_PL': 'Skopiowane !',
-        'it_IT': 'Copiato !'}
+        'fr': 'Copié !',
+        'de': 'Kopiert !',
+        'en': 'Copied !',
+        'es': 'Copiado !',
+        'pl': 'Skopiowane !',
+        'it': 'Copiato !'}
 };
 const ICON_COPY = '&#x2398';
-
-let last_page_viewed = "";
 
 console.log("Arte++ is running...");
 
@@ -120,44 +91,11 @@ console.log("Arte++ is running...");
     console.log("Observing...");
 
     getJSON();
-
-//     let nodeElement = ELEMENT_TO_OBSERVE;
-//     let config = { attributes: true, subtree: true };
-
-//     let callback = function(mutationsList) {
-
-//         for(var mutation of mutationsList) {
-
-//             if (mutation.target.className == 'video-thumbnail') {
-
-//                 if (last_page_viewed != mutation.target.baseURI) {
-
-//                     console.log("Page changed : "+ window.location.pathname);
-
-//                     if (mutation.target.baseURI.match(REGEX)) {
-//                         console.log("OK");
-//                         last_page_viewed = mutation.target.baseURI;
-//                         getJSON();
-//                     }
-
-//                 }
-
-//             }
-
-//         }
-
-//     };
-
-//     let observer = new MutationObserver(callback);
-//     observer.observe(nodeElement, config);
 })();
 
 function getJSON() {
-    let location = window.location.pathname.split("/");
-    let lang = location[1];
-    let video_id = location[3];
-    let api_base_url = ARTE_API + lang + "/";
-    let api_url = api_base_url + video_id;
+    let api_base_url = ARTE_API + LANG + "/";
+    let api_url = api_base_url + VIDEO_ID;
 
     fetch(api_url)
         .then(
@@ -170,118 +108,51 @@ function getJSON() {
 }
 
 function getLinks(json) {
-    let video_data = json.videoJsonPlayer.VSR;
+    let video_data = json.data.attributes.streams;
     let video_keys = Object.keys(video_data);
-    let parsed_data = [];
     let array_to_return = [];
 
     Array.from(video_keys).forEach((key) => {
         let data = video_data[key];
 
-        if (data.mimeType.startsWith("video") === false) {
-            return;
-        }
-
-        let keep = {
-            title: data.versionLibelle,
-            code: data.versionCode,
-            country_code: data.versionShortLibelle,
+        array_to_return.push({
             url: data.url,
-            width: data.width,
-            height: data.height,
-            bitrate: data.bitrate,
-            extension: data.mediaType
-        };
-
-        parsed_data.push(keep);
+            label: data.versions[0].label
+        });
     });
-
-    array_to_return.videos = parsed_data;
-
-    // add lang of website to show right translations :)
-    array_to_return.isoLang = json.videoJsonPlayer.videoIsoLang;
 
     showLinks(array_to_return);
 }
 
+
 function showLinks(data) {
 
-    let versions = data.videos.sort(sortingByLang);
-    let parent = document.querySelector(PARENT_TO_APPEND).parentElement;
-    let content = document.createElement("div");
-    let ul = document.createElement("div");
-    let old_label = "";
+    ELEMENT_TO_OBSERVE.insertAdjacentHTML('afterend', '<div id="arteplusplus"></div>');
+    let div = document.querySelector("#arteplusplus");
+    div.setAttribute('class',div.previousSibling.getAttribute('class'));
+    div.innerHTML = "<p><b>Copy and paste one of the links below with youtube-dl software.</b>It will download the stream of the audio & the video, then merge both into a .mp4 file. You'll also need ffmpeg to do the merging.<br><br>";
+    div.innerHTML += "Youtube-dl downlopad page: <a href=\"https://github.com/ytdl-org/youtube-dl\">https://github.com/ytdl-org/youtube-dl</a><br>FFmpeg download page: <a href=\"https://ffmpeg.org/download.html\">https://ffmpeg.org/download.html</a><br><br>";
+    div.innerHTML += "arte.tv doesn't provide anymore link to a .mp4 file. :)<br><br>";
 
-    parent.after(content);
-
-    content.insertAdjacentHTML('beforeend', '<h1>'+LOCALE.downloads[data.isoLang]+'</h1>');
-    content.classList.add("arte-plus-plus");
-    ul.classList.add("row");
-
-    versions.forEach( function(element) {
-
-        let label = element.title;
-
-        if (label != old_label || old_label == "") {
-
-            if (label != old_label && old_label != "") {
-                content.appendChild(ul);
-                ul = document.createElement("div");
-                ul.classList.add("row");
-            }
-
-            let subtitle = document.createElement("h2");
-
-            content.appendChild(subtitle);
-            subtitle.insertAdjacentHTML('beforeend', label);
-
-            old_label = label;
-        }
-
-        ul.insertAdjacentHTML('beforeend', '<div class="columns small-12 medium-6 large-3 "><div class="link-plus-plus"><a class="copied" target="_blank" title="'+element.title+'" href="'+element.url+'">'+element.extension.toUpperCase()+' ('+element.width+'x'+element.height+')</a><span class="copypaste">'+ICON_COPY+' '+LOCALE.copy[data.isoLang]+'</span></div></div>');
-
+    data.forEach((key) => {
+        div.innerHTML += `<div><b>`+key.label+`</b> : <input style="width:100%;" class="arteplusplus_link" type="text" value="`+encodeURI(key.url)+`"></div>`
     });
 
-    content.appendChild(ul);
+    div.innerHTML += "</p>";
 
-    let copypaste = document.querySelectorAll('.copypaste');
-    let last_copied = [];
+    let inputs = document.querySelectorAll('.arteplusplus_link');
 
-    Array.from(copypaste).forEach(link => {
+    Array.from(inputs).forEach(link => {
 
         link.addEventListener('click', function(event) {
-            console.log(last_copied);
-
-            if (last_copied[0] != undefined) {
-                clearTimeout(last_copied[0]);
-                copied(last_copied[1], ICON_COPY+' '+LOCALE.copy[data.isoLang], false);
-            }
-
-            copied(link, ICON_COPY+' '+LOCALE.copied[data.isoLang], true);
-
-            last_copied = [setTimeout(function(timer){
-                copied(link, ICON_COPY+' '+LOCALE.copy[data.isoLang], false);
-            }, 400), link];
-
-            GM_setClipboard(link.previousSibling.href, "text")
-
+            GM_setClipboard(link.value, "text")
+            link.classList.add("copied");
+            let temp = link.value
+            link.value = LOCALE.copied[LANG]
+            setTimeout(function(timer){
+                link.classList.remove("copied");
+                link.value = temp
+            }, 400)
         });
     });
-}
-
-function sortingByLang (a, b) {
-    if (a.code=== b.code) {
-        return b.bitrate - a.bitrate;
-    }
-    return a.code > b.code ? 1 : -1;
-}
-
-function copied(element, message, bool) {
-    if (bool) {
-        element.innerHTML = message;
-        element.classList.add("copied");
-    } else {
-        element.innerHTML = message;
-        element.classList.remove("copied");
-    }
 }
